a7690aeb66bf7e76a27b4eafe00a922b9c42bd31
AlbertooMM <ertortu2006@gmail.com>
2026-04-11 12:48:26 +0200

    Comentarios1
    Comentarios descriptivos en las clases DAO

--- src/main/resources/archetype-resources/src/main/java/Dao/Conexion.java
+++ src/main/resources/archetype-resources/src/main/java/Dao/Conexion.java
@@ -3,13 +3,14 @@ package main.java.Dao;
 import java.sql.*;
 
 public class Conexion {
-    private static final String URL = "jdbc:mariadb://localhost:3306/company_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "123456";
+    private static final String URL = "jdbc:mariadb://localhost:3306/company_db"; // Direccion del servidor
+    private static final String USER = "root"; // Usuario con la que se accede a la BBDD
+    private static final String PASSWORD = "123456";// Contraseña del usuario
     private static Connection conn = null;
     Statement st = null;
     ResultSet rs = null;
 
+    // Método por el que se conecta a la BBDD
     public static Connection conectar() {
         try {
             return DriverManager.getConnection(URL, USER, PASS); // Devuelve el estado de la conexion de la base de datos
@@ -20,7 +21,7 @@ public class Conexion {
     }
 
 }
-
+// Cerrar la conexion a la base de datos
     public static void desconectar() {
         if  (conn!= null ){
             conn.close();
--- src/main/resources/archetype-resources/src/main/java/Dao/HabitacionDAO.java
+++ src/main/resources/archetype-resources/src/main/java/Dao/HabitacionDAO.java
@@ -4,18 +4,22 @@ package main.java.Dao;
 public class HabitacionDAO {
 
     public List<Habitacion> listarHabitaciones() {
-        List<Habitacion> habitaciones = new ArrayList<>();
+        List<Habitacion> habitaciones = new ArrayList<>();// Creacion de la lista habitaciones
+
+        //Sentencia sql
         String sql = """
             SELECT h.id, h.id_hotel, h.tiempo_alquiler, h.capacidad, h.precio
             FROM habitacion h
             JOIN hotel ho ON h.id_hotel = ho.id
             """;
 
+        // Uso de la sentencia sql en la base de datos
         try (Connection conn = ConexionBD.getConexion();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
 
             while (rs.next()) {
+                // Se añade los campos de la base de datos y se añade al objeto hab
                 Habitacion hab = new Habitacion(
                         rs.getInt("id"),
                         rs.getInt("id_hotel"),
@@ -23,11 +27,14 @@ public class HabitacionDAO {
                         rs.getInt("capacidad"),
                         rs.getDouble("precio")
                 );
+
+                // Se añade la clase habitacion a la lista
                 habitaciones.add(hab);
             }
         } catch (SQLException e) {
             System.err.println("Error listando habitaciones: " + e.getMessage());
         }
+        //Devuelve la lista
         return habitaciones;
     }
 
