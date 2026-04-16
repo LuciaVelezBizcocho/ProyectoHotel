40788af0fd27e2cce00300cf06089ff5cb518cab
Lucía Vélez Bizcocho <luciavebi.20@gmail.com>
2026-04-16 12:28:57 +0200

    Código para la clase Menú que al final no he editado y lo he dejado como esta

--- src/main/java/MainProyecto/Main.java
+++ src/main/java/MainProyecto/Main.java
@@ -1,4 +1,15 @@
 package MainProyecto;
 
+import DAO.HabitacionDAO;
+import Inicial.Menu;
+import Servicio.GestionUsuarioServicio;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import static Inicial.Menu.mostrarMenu;
+
 public class Main {
+
 }
