000c15d081063fc1b3caa237fb48c5c0c7c9b9c7
RAntonio-Web <correamamaniantonioramiro@gmail.com>
2026-04-16 11:11:31 +0200

    Cmabios nuevos 3.0

--- src/main/java/DAO/HabitacionDAO.java
+++ src/main/java/DAO/HabitacionDAO.java
@@ -1,4 +1,39 @@
 package DAO;
 
+import DAO.ConexionBD;
+import Modelo.Habitacion;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HabitacionDAO {
+
+    public List<Habitacion> listarHabitaciones() {
+        List<Habitacion> habitaciones = new ArrayList<>();
+        String sql = """
+            SELECT h.id, h.id_hotel, h.tiempo_alquiler, h.capacidad, h.precio
+            FROM habitacion h
+            JOIN hotel ho ON h.id_hotel = ho.id
+            """;
+
+        try (Connection conn = ConexionBD.getConexion();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Habitacion hab = new Habitacion(
+                        rs.getInt("id"),
+                        rs.getInt("id_hotel"),
+                        rs.getInt("tiempo_alquiler"),
+                        rs.getInt("capacidad"),
+                        rs.getDouble("precio")
+                );
+                habitaciones.add(hab);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error listando habitaciones: " + e.getMessage());
+        }
+        return habitaciones;
+    }
 }
