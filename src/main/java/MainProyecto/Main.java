7ef25614fd6c3031a8b25c1cef21b4a9db068a6c
Lucía Vélez Bizcocho <luciavebi.20@gmail.com>
2026-04-17 13:12:18 +0200

    Commit de comentario

--- src/main/java/MainProyecto/Main.java
+++ src/main/java/MainProyecto/Main.java
@@ -16,6 +16,10 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        //Comentario por la impresión de nombre
+
+
         Map<Integer, Boolean> estadoHabitaciones = new HashMap<>();
         insertarDatosEnMapa(estadoHabitaciones);
 
