907a6d5d2c2969a429f2110b2b9c089e34f1b71f
AlbertooMM <ertortu2006@gmail.com>
2026-04-28 18:58:03 +0200

    Optimizacion de la clase conexionBD

--- src/main/java/DAO/ConexionBD.java
+++ src/main/java/DAO/ConexionBD.java
@@ -3,22 +3,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class ConexionBD {
     private static final String URL = "jdbc:mariadb://localhost:3306/gestion_hoteles";
     private static final String USER = "root";
     private static final String PASS = "123456"; // Cambiar por tu contraseña
 
     public static Connection getConexion() {
+
         try {
-            Class.forName("org.mariadb.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASS);
-        } catch (ClassNotFoundException e) {
-            System.err.println("Driver no encontrado: " + e.getMessage());
-            return null;
         } catch (SQLException e) {
-            System.err.println("Error de conexión: " + e.getMessage());
+            System.out.println("Error al conectar con la base de datos");
             return null;
         }
+
     }
 
 
